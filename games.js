a0e92617ee2fd1e80db0c62aa22e29522326ec5a
Anton Nikonov <anick.on.off@gmail.com>
2025-07-09 08:33:19 +0700

    add game window and change scripts

--- games.js
+++ games.js
@@ -1,37 +1,59 @@
+//all games
+ 
+function showGameWindow(id) {
+    const popupOverlay = document.getElementById(id);
+    popupOverlay.style.display = "block";
+}
+function hideGameWindow(id) {
+    const popupOverlay = document.getElementById(id);
+    popupOverlay.style.display = "none";
+}
+
+
+
+// Game1
+let secretNumber;
+let count;
 function game1() {
-    const secretNumber = Math.floor(Math.random() * 100);
-    let count = 0;
-    while(true) {
-        let userNumber = prompt('Угадайте число');
-        if (userNumber === null) {
-            break;
-        }
-        if (isNaN(Number(userNumber)) || userNumber === '') {
-            alert('Переданный параметр не является числом!');
-            continue;
-        }
-        if (userNumber < secretNumber) {
-            alert('Загаданное число больше, попробуйте ещё раз.');
-            count++;
-        } else if (userNumber > secretNumber) {
-            alert('Загаданное число меньше, попробуйте ещё раз.');
-            count++;
-        } else {
-            count++;
-            alert(`Поздравляю, Вы угадали число ${userNumber} за ${count} ${endingTryWord(count)}!!!`);
-            break;
-        }
+  secretNumber = Math.floor(Math.random() * 100);
+  count = 0;
+}
+
+function chkNumber() {
+  let userNumber = document.getElementById('game__input1').value;
+    let userText = '<p class="sug">Вы ввели ' + userNumber + "</p>";
+    addText(userText);
+    if (isNaN(Number(userNumber)) || userNumber === "") {
+      addText("Переданный параметр не является числом!");
+    } else if (userNumber < secretNumber) {
+      //alert('Загаданное число больше, попробуйте ещё раз.');
+      addText('<p class="ansver">Загаданное число больше, попробуйте ещё раз.</p>');
+      count++;
+    } else if (userNumber > secretNumber) {
+      //  alert('Загаданное число меньше, попробуйте ещё раз.');
+      addText('<p class="ansver"> Загаданное число меньше, попробуйте ещё раз.</p>');
+      count++;
+    } else {
+      count++;
+      userText = '<p class="ansver">Поздравляю, Вы угадали число ' + userNumber + ' за ' + count + ' ' + endingTryWord(count) + "!!!</p>";
+    addText(userText);
     }
 }
 
 function endingTryWord(a) {
-    if (a > 10 && a < 20) {
-        return 'попыток';        
-    } else if (a % 10 > 1 && a % 10 < 5) {
-        return 'попытки';        
-    } else if (a % 10 > 4 && a % 10 <= 9 || a % 10 == 0 ) {
-        return 'попыток';
-    } else {
-        return 'попытку';
-    }
+  if (a > 10 && a < 20) {
+    return "попыток";
+  } else if (a % 10 > 1 && a % 10 < 5) {
+    return "попытки";
+  } else if ((a % 10 > 4 && a % 10 <= 9) || a % 10 == 0) {
+    return "попыток";
+  } else {
+    return "попытку";
+  }
 }
+
+function addText(text) {
+  let div = document.getElementById('game__textfield1');
+  div.innerHTML += text;
+}
+
