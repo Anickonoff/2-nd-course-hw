3ce7c83e08b3cee1363b93b0a280345027dfe365
Anton Nikonov <anick.on.off@gmail.com>
2025-08-11 12:52:05 +0700

    scripts for tasks

--- script.js
+++ script.js
@@ -1,81 +1,76 @@
 //Задание 1
 
-let str1 = 'js';
-let newStr1 = str1.toUpperCase();
-console.log(newStr1);
+const people = [
+   { name: 'Глеб', age: 29 },
+   { name: 'Анна', age: 17 },
+   { name: 'Олег', age: 7 },
+   { name: 'Оксана', age: 47 }
+];
+console.log(people.sort((a, b) => a.age - b.age));
 
 //Задание 2
 
-function strFilter(strs, str) {
-  const filteredStrs = [];
-  strs.forEach((el) => {
-    if (el.toLowerCase().startsWith(str.toLowerCase())) {
-      filteredStrs.push(el);
-    }
-  });
-  return filteredStrs;
+function isPositive(item) {
+  return item > 0 ? true : false;
 }
+function isMale(item) {
+  return item.gender === 'male'? true : false;
+}
+function filter(arr, filterFunction) {
+  const newArr = [];
+  arr.forEach((el) => {
+    if (filterFunction(el)) newArr.push(el);
+  })
+  return newArr;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const people = [
+   {name: 'Глеб', gender: 'male'},
+   {name: 'Анна', gender: 'female'},
+   {name: 'Олег', gender: 'male'},
+   {name: 'Оксана', gender: 'female'}
+];
+
+console.log(filter(people, isMale));
 
 //Задание 3
 
-let number3 = 32.58884;
-console.log(`Число ${number3} округленное до меньшего целого равно ${Math.floor(number3)}`);
-console.log(`Число ${number3} округленное до большего целого равно ${Math.ceil(number3)}`);
-console.log(`Число ${number3} округленное до ближайшего целого равно ${Math.round(number3)}`);
+function currentDate() {
+  let timer = setInterval(() => {
+    let date = new Date();
+    console.log(date);
+  }, 3000);
+  setTimeout(() => {
+    clearInterval(timer);
+    console.log('30 секунд прошло');
+  }, 30000);
+}
 
 //Задание 4
 
-const numbers4 = [52, 53, 49, 77, 21, 32];
-console.log(`Минимальное число массива [${numbers4}] равно ${Math.min(...numbers4)}`);
-console.log(`Максимальное число массива [${numbers4}] равно ${Math.max(...numbers4)}`);
+function delayForSecond(callback) {
+  setTimeout(callback, 1000);
+}
+
+delayForSecond(function () {
+  console.log('Привет, Глеб!');
+})
 
 //Задание 5
 
-const random10 = () => Math.ceil(Math.random() * 10);
-//Либо с использованием math.floor согласно требованиям:
-const newRandom10 = () => Math.floor(Math.random() * 10) + 1;
-
-//Задание 6
-
-function randomArr(length) {
-  let arr = [];
-  //Длина массива равна половине числа с округлением в большую сторону
-  for (let i = 0; i < length / 2; i++) {
-    arr.push(Math.floor(Math.random() * length));
-  }  
-  return arr;
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
 }
 
-//Задание 7 
-
-const randomMinMax = (min, max) => Math.floor(Math.random() * (max - min) + min);
-
-//Задание 8
-
-console.log(Date());
-
-//Задание 9
-
-let currentDate = new Date();
-console.log(currentDate);
-//Через изменение UNIX времени
-let days73 = 73 * 24 * 3600 * 1000;
-console.log(new Date(+currentDate + days73));
-//Через суммирование дней, более громозко, но тоже работает
-let futureDate = new Date(currentDate.getFullYear(),currentDate.getMonth(),currentDate.getDate() + 73, currentDate.getHours(), currentDate.getMinutes(),currentDate.getSeconds(),currentDate.getMilliseconds());
-console.log(futureDate);
-//Или проще через setDate (в уроке и конспекте не упоминается)
-let futureDate2 = currentDate;
-futureDate2.setDate(futureDate2.getDate() + 73);
-console.log(futureDate2);
-
-//Задание 10
-
-function displayDate(date) {
-  const nameMonth = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
-  const nameDay = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-  let dateText = 'Дата: ' + date.getDate() + ' ' + nameMonth[date.getMonth()] + ' ' + date.getFullYear() + '— это ' + nameDay[date.getDay()];
-  let timeText = 'Время: ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-  return dateText + '\n' + timeText;
+// Чтобы код заработал, нужно было заменить ' на `. Иначе не подставляет значение переменной name
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
 }
+
+delayForSecond((item) => {return sayHi('Глеб')});
 
